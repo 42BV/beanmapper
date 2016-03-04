d59053361cf93a29ea990abebf83f6c06b9e6180
Rickvanderwaal <rickwaal@hotmail.com>
2016-03-04 09:43:45 +0100

    Initial commit new documentation, gh-pages. - Added bootstrap & jquery - First draft for layout

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,44 @@
-console.log('This would be the main JS file.');
+$(document).ready(function () {
+
+    var menu = $('#menu');
+
+    /**
+     * Function to set fixed navigation after scrolling.
+     */
+    $(window).scroll(function () {
+        var window_top = $(window).scrollTop();
+        if(window_top > $('header').height()) {
+            menu.addClass('stick');
+        } else {
+            menu.removeClass('stick');
+        }
+    });
+
+    /**
+     * Function that causes smooth scrolling.
+     */
+    menu.find("a").click(function () {
+        var target = $(this.hash);
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 750);
+    });
+
+    /**
+     * Function that handles highlighting of menu items.
+     */
+    $(window).scroll(function () {
+        var windowPos = $(window).scrollTop();
+
+        menu.find("a").each(function() {
+            var sectionByLink = $($(this).attr('href'));
+            var divPos = sectionByLink.offset().top;
+            var divHeight = sectionByLink.height();
+            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                $(this).addClass("active");
+            } else {
+                $(this).removeClass("active");
+            }
+        });
+    });
+});
