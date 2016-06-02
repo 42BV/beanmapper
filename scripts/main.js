3221ad6ef067fe4d39245f5ba815c309caa09a05
Rick van der Waal <rickwaal@hotmail.com>
2016-06-02 17:07:38 +0200

    Added fontawesome & updated beanmapper documentation

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,15 @@
 $(document).ready(function () {
-
     $('code').each(function(i, block) {
         hljs.highlightBlock(block);
-    });
-    var menu = $('#menu');
-    menu.css('max-height', window.innerHeight);
-
-    /**
-     * Function to set fixed navigation after scrolling.
-     */
-    $(window).scroll(function () {
-        var window_top = $(window).scrollTop();
-        if(window_top > $('header').height()) {
-            menu.addClass('stick');
-        } else {
-            menu.removeClass('stick');
-        }
     });
 
     /**
      * Function that causes smooth scrolling.
      */
-    menu.find("a").click(function () {
+    $('.sidebar').find("a").click(function () {
         var target = $(this.hash);
         $('html, body').animate({
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top-50
         }, 750);
     });
 
@@ -34,19 +19,15 @@ $(document).ready(function () {
     $(window).scroll(function () {
         var windowPos = $(window).scrollTop()+50; // Look 50px ahead
 
-        menu.find("a").each(function() {
+        $('.sidebar').find("a").each(function() {
             var sectionByLink = $($(this).attr('href'));
             var divPos = sectionByLink.offset().top;
             var divHeight = sectionByLink.height();
             if (divPos <= windowPos && (divPos + divHeight) > windowPos) {
-                $(this).addClass("active");
+                $(this).parent().addClass("active");
             } else {
-                $(this).removeClass("active");
+                $(this).parent().removeClass("active");
             }
         });
     });
-});
-
-$(window).resize(function() {
-    $('#menu').css('max-height', window.innerHeight);
 });
